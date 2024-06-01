cf26cfbc28e98ad55ecaef2c267fef283f10451d
“[keshav <123925652+keshavsinha17@users.noreply.github.com>
2024-06-01 23:54:32 +0530

    ui/home description

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 import "./Home.css";
 const Home = () => {
   return (
-    <div>
-      <h2>Home Page</h2>
+    <div className='home'>
+      <div className='hero'>
+      <h1>Virtual<br/>Crypto MarketPlace</h1>
+      <p>Welcome to Forge of doing Dummy Crypto Transaction and Trades with real risk involved 
+      without putting your credits at stake.Sign up now!!</p>
+<form>
+<input type='text' placeholder='Search Coin'></input>
+<button type='submit'>Search</button>
+</form>
+      </div>
     </div>
   )
 }
