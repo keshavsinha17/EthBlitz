ceeae7a247bb098fcda2d4e0c150ec7b644f1995
“[keshav <123925652+keshavsinha17@users.noreply.github.com>
2024-06-02 00:04:47 +0530

    ui/layout-table

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,19 +1,29 @@
 import React from 'react'
 import "./Home.css";
 const Home = () => {
-  return (
-    <div className='home'>
-      <div className='hero'>
-      <h1>Virtual<br/>Crypto MarketPlace</h1>
-      <p>Welcome to Forge of doing Dummy Crypto Transaction and Trades with real risk involved 
-      without putting your credits at stake.Sign up now!!</p>
-<form>
-<input type='text' placeholder='Search Coin'></input>
-<button type='submit'>Search</button>
-</form>
-      </div>
-    </div>
-  )
+    return (
+        <div className='home'>
+            <div className='hero'>
+                <h1>Virtual<br />Crypto MarketPlace</h1>
+                <p>Welcome to Forge of doing Dummy Crypto Transaction and Trades with real risk involved
+                    without putting your credits at stake.Sign up now!!</p>
+                <form>
+                    <input type='text' placeholder='Search Coin'></input>
+                    <button type='submit'>Search</button>
+                </form>
+            </div>
+            <div className='crypto-table'>
+                <div className='table-layout'>
+                    <p>#</p>
+                    <p>Coins</p>
+                    <p>Price</p>
+                    <p style={{textAlign:'center'}}>24hr Change</p>
+                    <p className='market-cap'>Market Cap</p>
+                </div>
+
+            </div>
+        </div>
+    )
 }
 
 export default Home
